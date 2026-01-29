2f77755fd64735a4b676785ca4265da029dd9a27
GetulioHF <getulio@gfs.com.br>
2026-01-29 08:22:56 -0500

    commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,18 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //Write your code here (GetulioHF)
+  const booksJson = JSON.stringify(books, null, 2);
+  return res.status(300).send(booksJson);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  const bookJson = JSON.stringify(book, null, 2);
+  return res.status(300).send(bookJson);
  });
   
 // Get book details based on author
