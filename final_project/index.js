21b6165cfbcdb339e626ae9ee2c4a0cdf3338c53
GetulioHF <getulio@gfs.com.br>
2026-01-29 15:57:27 -0500

    a

--- final_project/index.js
+++ final_project/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');   // GetulioHF
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
